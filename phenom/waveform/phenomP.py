37c172f5ab0ba7c2d4190912560a1943e56dd9b0
Sebastian.Khan <sebastian.khan@LIGO.org>
2016-07-09 10:49:03 +0100

    phenomP file. nothing here yet

---
+++ phenom/waveform/phenomP.py
@@ -0,0 +1,4 @@
+class PhenomP(Waveform, PhenomDInternals):
+    """docstring for PhenomD"""
+    def __init__(self, m1=10., m2=10., chi1z=0., chi2z=0., f_min=20., f_max=0., delta_f=1/64., distance=1e6 * Constants.PC_SI, fRef=0., phiRef=0.):
+        pass
