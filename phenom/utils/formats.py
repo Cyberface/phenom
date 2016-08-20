4bef1669066e08038b8528578559379e0902a049
Sebastian.Khan <sebastian.khan@LIGO.org>
2016-08-20 17:51:06 +0100

    added flexibility in makewaveformseries

--- phenom/utils/formats.py
+++ phenom/utils/formats.py
@@ -1,14 +1,23 @@
-from numpy import arange
+from numpy import linspace
 from scipy.interpolate import interp1d
 
 class MakeWaveformSeries(object):
     """docstring for MakeWaveformSeries"""
-    def __init__(self, freqs, hptilde, hctilde, df=None):
+    def __init__(self, freqs, hptilde, hctilde, f_min=None, f_max=None, df=None):
         """
         assuming uniformly spaced!
         """
-        self.f_min = freqs[0]
-        self.f_max = freqs[-1]
+
+        if f_min is None:
+            self.f_min = freqs[0]
+        else:
+            self.f_min = f_min
+        if f_max is None:
+            self.f_max = freqs[-1]
+        else:
+            self.f_max = f_max
+
+
 
         if df is None:
             #TODO: Add option to interpolate to unitformly spaced?
@@ -21,9 +30,7 @@ class MakeWaveformSeries(object):
             self.df = df
             interp_hptilde = interp1d(freqs, hptilde)
             interp_hctilde = interp1d(freqs, hctilde)
-            self.flist_Hz = arange(self.f_min, self.f_max, self.df)
+            self.npts = (self.f_max-self.f_min)/self.df
+            self.flist_Hz = linspace(self.f_min, self.f_max, self.npts)
             self.hptilde = interp_hptilde(self.flist_Hz)
             self.hctilde = interp_hctilde(self.flist_Hz)
-            self.npts = len(self.flist_Hz)
-
-            
