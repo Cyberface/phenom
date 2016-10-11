improve planck taper. still to fix overflow exp

phenom/utils/utils.py
@@ -374,15 +374,20 @@ def planck_taper(tlist, t1, t2):
     t1. for t<=t1 then return 0
     t2. for t>=t2 then return 1
     else return 1./(np.exp((t2-t1)/(t-t1)+(t2-t1)/(t-t2))+1)"""
-    tout = []
-    for t in tlist:
+
+    N = len(tlist)
+
+    tout = np.zeros( N )
+
+    for i, t in enumerate(tlist):
         if t<=t1:
-            tout.append(0.)
+            tout[i] = 0.
         elif t>=t2:
-            tout.append(1.)
+            tout[i] = 1.
         else:
-            tout.append(1./(exp((t2-t1)/(t-t1)+(t2-t1)/(t-t2))+1))
-    return asarray(tout)
+            FAC = ( (t2-t1) / (t-t1) ) + ( (t2-t1) / (t-t2) )
+            tout[i] = 1. / ( np.exp( FAC ) + 1. )
+    return np.asarray(tout)
 
 def CartToPolar(chi1x, chi1z):
     """CartToPolar(chi1x, chi1z)