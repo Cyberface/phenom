1dd373fd3c2fd920b7f6934340d29e3f66b86fe1
Sebastian Khan <Sebastian.Khan@LIGO.org>
2018-05-28 21:07:23 +0200

    change to 3 panel plot

--- examples/prec-angles/compare-pv2-pv3.py
+++ examples/prec-angles/compare-pv2-pv3.py
@@ -40,7 +40,7 @@ f_gw_list.data = np.arange(f_gw_min, f_gw_max, df_gw)
 
 
 
-m1,m2 =m1_m2_M_q(20., 10)
+m1,m2 =m1_m2_M_q(20., 3)
 
 m1=m1*lal.MSUN_SI
 m2=m2*lal.MSUN_SI
@@ -90,28 +90,48 @@ pv2angles['alpha'],pv2angles['beta'],pv2angles['epsilon']= pah.evaluate_phenomPv
 
 import matplotlib.pyplot as plt
 
-plt.figure()
-plt.plot(f_gw_list.data, pv2angles['alpha'], label='pv2')
-plt.plot(f_gw_list.data, pv3angles['alpha'], label='pv3')
-plt.title("alpha")
-plt.legend()
-plt.xscale('log')
-plt.show()
+# plt.figure()
+# plt.plot(f_gw_list.data, pv2angles['alpha'], label='pv2')
+# plt.plot(f_gw_list.data, pv3angles['alpha'], label='pv3')
+# plt.title("alpha")
+# plt.legend()
+# plt.xscale('log')
+# plt.show()
 
-plt.figure()
-plt.plot(f_gw_list.data, pv2angles['beta'], label='pv2')
-plt.plot(f_gw_list.data, pv3angles['beta'], label='pv3')
-plt.title("beta")
-plt.legend()
-plt.yscale('log')
-plt.xscale('log')
-plt.show()
+# plt.figure()
+# plt.plot(f_gw_list.data, pv2angles['beta'], label='pv2')
+# plt.plot(f_gw_list.data, pv3angles['beta'], label='pv3')
+# plt.title("beta")
+# plt.legend()
+# plt.yscale('log')
+# plt.xscale('log')
+# plt.show()
 
 
-plt.figure()
-plt.plot(f_gw_list.data, pv2angles['epsilon'], label='pv2')
-plt.plot(f_gw_list.data, pv3angles['epsilon'], label='pv3')
-plt.title("epsilon")
-plt.legend()
-plt.xscale('log')
+# plt.figure()
+# plt.plot(f_gw_list.data, pv2angles['epsilon'], label='pv2')
+# plt.plot(f_gw_list.data, pv3angles['epsilon'], label='pv3')
+# plt.title("epsilon")
+# plt.legend()
+# plt.xscale('log')
+# plt.show()
+
+fig, axes = plt.subplots(1,3, figsize=(14,4))
+
+axes[0].plot(f_gw_list.data, pv2angles['alpha'], label='pv2')
+axes[0].plot(f_gw_list.data, pv3angles['alpha'], label='pv3')
+axes[0].set_title("alpha")
+axes[0].set_xscale('log')
+
+axes[1].plot(f_gw_list.data, pv2angles['beta'], label='pv2')
+axes[1].plot(f_gw_list.data, pv3angles['beta'], label='pv3')
+axes[1].set_title("beta")
+axes[1].set_yscale('log')
+axes[1].set_xscale('log')
+
+axes[2].plot(f_gw_list.data, pv2angles['epsilon'], label='pv2')
+axes[2].plot(f_gw_list.data, pv3angles['epsilon'], label='pv3')
+axes[2].set_title("epsilon")
+axes[2].set_xscale('log')
+axes[2].legend()
 plt.show()
