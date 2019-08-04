0b11e18fe4fe4cd5a6da35a8a921e5876a56e45d
Jonathan Thompson <thompson.phys@gmail.com>
2019-08-04 12:10:50 +0100

    adding PhenomPv2 'modes' class

--- phenom/waveform/__init__.py
+++ phenom/waveform/__init__.py
@@ -2,3 +2,4 @@ from phenom.waveform.waveform import *
 from phenom.waveform.phenomD import *
 from phenom.waveform.phenomD_mod import *
 from phenom.waveform.phenomP import *
+from phenom.waveform.phenomP_modes import *
